5d46b9bb4e59b6ad61b54a5c3cd82c12c76e0ca9
root <root@Moh.localdomain>
2024-01-17 19:24:26 +0300

    try modified init basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -21,22 +21,17 @@ class BaseModel:
                         )
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            for key, value in kwargs.items():
-                key = key.replace("_", " ")
-                if not hasattr(self, key):
-                    setattr(self, key, value)
-            self.__dict__.update(kwargs)
+        self.id = str(uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = self.created_at
+        
+        if kwargs:
+            for key, val in kwargs.items():
+                if key in ("created_at", "updated_at"):
+                    val = datetime.strptime(kwargs['updated_at'],
+                                            '%Y-%m-%dT%H:%M:%S.%f')
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def __str__(self):
         """Returns a string representation of the instance"""
