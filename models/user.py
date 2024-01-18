0864748aab003d2bb0f15d961875d764972d6879
KenNyagz <kennyagz681@gmail.com>
2024-01-19 00:57:36 +0300

    jjj

--- models/user.py
+++ models/user.py
@@ -4,22 +4,18 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
 from os import getenv
 from sqlalchemy.orm import relationship
-from models.review import Review
-from models.review import Review
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
+        places = relationship("Place", backref="user", cascade="all, delete-orphan")
+        reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
-        reviews = relationship('Review',
-                               cascade='all,delete-orphan',
-                               backref='user')
-        places = relationship('Place', backref='user', cascade='all, delete-orphan')
 
     else:
         email = ''
