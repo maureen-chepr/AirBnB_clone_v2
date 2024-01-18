284acc1bcc62806f2d6e7499ba2405a7a3eab9ad
root <root@Moh.localdomain>
2024-01-19 00:36:32 +0300

    no 9

--- models/place.py
+++ models/place.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy.orm import relationship
+from os import getenv
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    if os.getenv ('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = "places"
-        city_id = Column(String(60), nullable-False, ForeignKey('cities.id')
-        user_id = Column(String(60), nullable=False, ForeignKey('users.id')
+    __tablename__ = "places"
+    if getenv ('HBNB_TYPE_STORAGE') == 'db':
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=False)
         number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, defalut=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
         price_by_night = Column(Integer, nullable=False,default=0)
-        latitude = Column(Float, nullable=False)
-        longitude= Column(Float, nullable=False)
+        latitude = Column(Float, nullable=True)
+        longitude= Column(Float, nullable=True)
+        reviews = relationship("Review", backref="place", cascade="delete")
 
     else:
         city_id = ""
@@ -30,3 +33,16 @@ class Place(BaseModel, Base):
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
+
+        @property
+        def reviews(self):
+            """
+                getter attribute reviews that returns
+                the list of Review instances
+            """
+            from models import storage
+            rev_lst = []
+            for review in list(models.storage.all(Review).values()):
+                if review.place_id == self.id:
+                    rev_lst.append(review)
+            return rev_lst
