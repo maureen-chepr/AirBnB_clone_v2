162f0f69da3334d02b41a11a514355d5e0c4494f
maureen-chepr <maureenchep95@gmail.com>
2024-02-01 00:08:42 -0800

    Fabric script that generates a .tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    script that generates a .tgz archive,
+    from the contents of the web_static
+"""
+from fabric.api import local
+from datetime import datetime
+import os.path
+
+def do_pack():
+    """generationg a tgz file"""
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_path = "versions/web_static_{}.tgz".format(date)
+    if os.path.isdir("versions") is False:
+        local(" mkdir versions")
+    local('tar -cvzf ' + file_path + ' web_static')
+    if os.path.exists(file_path):
+        return file_path
+    return None
