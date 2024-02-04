5cff57f1fa356b84c1ad887edd8061bedc6bf2f4
maureen-chepr <maureenchep95@gmail.com>
2024-02-04 10:33:12 -0800

    set up with puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,48 +1,30 @@
 # setting up web static using puppet
-
-exec {'apt-get-update':
-  command => '/usr/bin/apt-get update'
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package { 'nginx':
+  ensure => installed,
+}
+-> exec { 'run1':
+  command => '/usr/bin/mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"',
+}
+-> exec { 'run2':
+  command => '/usr/bin/echo "Hi!" | sudo tee /data/web_static/releases/test/index.html > /dev/null',
+}
+-> exec { 'run3':
+  command => '/usr/bin/rm -rf /data/web_static/current',
+}
+-> exec { 'run4':
+  command => '/usr/bin/ln -s /data/web_static/releases/test/ /data/web_static/current',
+}
+-> exec { 'run5':
+  command => '/usr/bin/chown -R ubuntu:ubuntu /data/',
+}
+-> exec { 'hbnb_static':
+  command => 'sudo sed -i "/^server {/a \ \n\tlocation \/hbnb_static {alias /data/web_static/current/;index index.html;}" /etc/nginx/sites-enabled/default',
+  provider => shell,
+}
+-> exec { 'run6':
+  command => '/usr/sbin/service nginx restart',
 }
 
-package {'apache2.2-common':
-  ensure  => 'absent',
-  require => Exec['apt-get-update']
-}
-
-package { 'nginx':
-  ensure  => 'installed',
-  require => Package['apache2.2-common']
-}
-
-service {'nginx':
-  ensure  =>  'running',
-  require => file_line['LOCATION SETUP']
-}
-
-file { ['/data', '/data/web_static', '/data/web_static/shared', '/data/web_static/releases', '/data/web_static/releases/test'] :
-  ensure  => 'directory',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  require =>  Package['nginx']
-}
-
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'present',
-  content => 'Holberton School,
-  require =>  Package['nginx']
-}
-
-file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test',
-  force  => true
-}
-
-file_line { 'LOCATION SETUP ':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-enabled/default',
-  line    => 'location /hbnb_static/ { alias /data/web_static/current/; autoindex off; } location / { ',
-  match   => '^\s+location+',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
